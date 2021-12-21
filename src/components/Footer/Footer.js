962b4f017e2eb5f596ddc922f657e5b3440b48a3
Mingui Bernardo <minguibernardo@gmail.com>
2021-12-21 11:28:12 +0100

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/rajshekhar26/cleanfolio'
+      href='https://github.com/minguibernardo/'
       className='link footer__link'
     >
      Created with love  in &nbsp; &nbsp;<Favorite className='icon-svg'/>Github
